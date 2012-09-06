2c448895694a88a1feb1d30e631287289782dc6d
Max Neklyudov <macscomp@gmail.com>
2012-09-06 16:22:05 +0400

    Add LM3S traps handler

---
+++ arch/arm/mach-lm3s1d21/traps.c
@@ -0,0 +1,17 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+
+#include <mach/hardware.h>
+
+/***************************************************************************/
+
+extern NORET_TYPE void die(const char *str, struct pt_regs *regs, int err);
+
+/***************************************************************************/
+
+NORET_TYPE void lm3s_die(struct pt_regs *regs)
+{
+	uint32_t regval = lm3s_getreg32(LM3S_SCB_INTCTRL);
+	uint32_t exception = (regval & SCB_INTCTRL_VECACT_MASK) >> SCB_INTCTRL_VECACT_SHIFT;
+	die("LM3S exception", regs, exception);
+}
