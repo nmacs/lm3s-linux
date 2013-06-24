20f60459f9de61594fabbac88232537af60f4a33
Max Neklyudov <macscomp@gmail.com>
2013-06-24 16:43:30 +0400

    Add stellaris adc driver

---
+++ arch/arm/plat-stellaris/adc.c
@@ -0,0 +1,194 @@
+//#define DEBUG
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/platform_device.h>
+#include <linux/mutex.h>
+
+#include <linux/proc_fs.h>
+
+#include <mach/hardware.h>
+#include <mach/pins.h>
+
+struct adc_device {
+	struct device *dev;
+	struct mutex lock;
+};
+
+static struct adc_device device;
+static struct proc_dir_entry *proc_entry;
+
+int adc_convert_ex(uint32_t* values, int *channels, int count)
+{
+	uint32_t actss;
+	uint32_t ssmux = 0;
+	uint32_t ssctl = ADC_SSCTL_END;
+	int ret = 0;
+	int i;
+
+	if (channels == NULL || values == NULL || count < 1 || count > 8)
+		return -EINVAL;
+
+	for (i = 0; ; i++, channels++) {
+		if (*channels == STLR_ADC_TEMPERATURE) {
+			ssctl |= ADC_SSCTL_TS;
+			dev_dbg(device.dev, "%s: capture temperature\n", __func__);
+		}
+		else {
+			ssmux |= *channels;
+			dev_dbg(device.dev, "%s: capture AIN%i\n", __func__, *channels);
+		}
+		
+		if ( (i + 1) >= count)
+			break;
+		
+		ssmux <<= 4;
+		ssctl <<= 4;
+	}
+	
+	if (mutex_lock_interruptible(&device.lock))
+		return -EINTR;
+	
+	adc_clock_ctrl(0, SYS_ENABLE_CLOCK);
+
+	actss = getreg32(STLR_ADC0_ACTSS);
+	actss &= ~ADC_ACTSS_ASEN0;
+	putreg32(actss, STLR_ADC0_ACTSS);
+
+	putreg32(ssmux, STLR_ADC0_SSMUX0);
+	putreg32(ssctl, STLR_ADC0_SSCTL0);
+
+	actss |= ADC_ACTSS_ASEN0;
+	putreg32(actss, STLR_ADC0_ACTSS);
+
+	dev_dbg(device.dev, "%s: sampling\n", __func__);
+	putreg32(ADC_PSSI_SS0, STLR_ADC0_PSSI);
+
+	while (getreg32(STLR_ADC0_ACTSS) & ADC_ACTSS_BUSY) {}
+
+	values += count - 1;
+	for (i = 0; i < count; ++i, --values)
+	{
+		uint32_t value = getreg32(STLR_ADC0_SSFIFO0) & ADC_SSFIFO_DATA_MASK;
+		dev_dbg(device.dev, "%s: read value %u\n", __func__, value);
+		*values = value;
+	}
+
+	adc_clock_ctrl(0, SYS_DISABLE_CLOCK);
+	
+	mutex_unlock(&device.lock);
+
+	return count;
+}
+EXPORT_SYMBOL_GPL(adc_convert_ex);
+
+int adc_convert(uint32_t *value, int channel)
+{
+	return adc_convert_ex(value, &channel, 1);
+}
+EXPORT_SYMBOL_GPL(adc_convert);
+
+static int read_proc(char *buf, char **start, off_t offset, int count, int *eof, void *data)
+{
+	uint32_t temperature, caps_voltage;
+	int res;
+	int channels[] = {14, 15, STLR_ADC_TEMPERATURE};
+	uint32_t values[ARRAY_SIZE(channels)];
+	
+	if (offset)
+		return 0;
+	
+	res = adc_convert_ex(values, channels, ARRAY_SIZE(channels));
+	if( res <= 0 )
+		return res;
+	
+	res = sprintf(buf, "caps-charge: %i mV, vunreg: %i mV temp: %i mC\n", 
+	              adc_to_mV(values[0]), adc_to_mV(values[1]) * 10, adc_to_mC(values[2]));
+	
+	*eof = 1;
+	
+	return res;
+}
+
+static int write_proc(struct file *file, const char __user *buf, unsigned long count, void *data)
+{
+	return count;
+}
+
+static int __devinit init_procfs(void)
+{
+	proc_entry = create_proc_entry("charge", 0666, NULL);
+	if (!proc_entry) {
+		printk(KERN_ERR "Error creating proc entry");
+		return -ENOMEM;
+	}
+	
+	proc_entry->read_proc = read_proc;
+	proc_entry->write_proc = write_proc;
+	
+	return 0;
+}
+
+static int stlr_adc_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	
+	device.dev = dev;
+	mutex_init(&device.lock);
+
+	platform_set_drvdata(pdev, &device);
+	
+	init_procfs();
+	
+	dev_dbg(dev, "%s probed\n", __func__);
+
+	return 0;
+}
+
+static int __devexit stlr_adc_remove(struct platform_device *pdev)
+{
+	//struct adc_device *adc = platform_get_drvdata(pdev);
+	return 0;
+}
+
+#ifdef CONFIG_PM
+static int stlr_adc_suspend(struct platform_device *pdev, pm_message_t state)
+{
+	//struct adc_device *adc = platform_get_drvdata(pdev);
+	return 0;
+}
+
+static int stlr_adc_resume(struct platform_device *pdev)
+{
+	//struct adc_device *adc = platform_get_drvdata(pdev);
+	return 0;
+}
+
+#else
+#define stlr_adc_suspend NULL
+#define stlr_adc_resume NULL
+#endif
+
+static struct platform_driver stlr_adc_driver = {
+	.driver		= {
+		.name	= "stellaris-adc",
+		.owner	= THIS_MODULE,
+	},
+	.probe		= stlr_adc_probe,
+	.remove		= __devexit_p(stlr_adc_remove),
+	.suspend	= stlr_adc_suspend,
+	.resume		= stlr_adc_resume,
+};
+
+static int __init adc_init(void)
+{
+	int ret;
+
+	ret = platform_driver_register(&stlr_adc_driver);
+	if (ret)
+		printk(KERN_ERR "%s: failed to add adc driver\n", __func__);
+
+	return ret;
+}
+
+arch_initcall(adc_init);
---
+++ arch/arm/plat-stellaris/include/mach/adc.h
@@ -0,0 +1,134 @@
+/************************************************************************************
+ * arch/arm/plat-stellaris/include/mach/lm3s_dma.h
+ *
+ *   Copyright (C) 2012 Max Nekludov. All rights reserved.
+ *   Author: Max Nekludov <macscomp@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ************************************************************************************/
+
+#ifndef __MACH_PLAT_STELLARIS_ADC_H
+#define __MACH_PLAT_STELLARIS_ADC_H
+
+#ifndef __ASSEMBLY__
+
+#define STLR_ADC_TEMPERATURE             (-1)
+
+static inline int adc_to_mV(uint32_t value)
+{
+	return (int)(3300U * value / 4096U);
+}
+
+static inline int adc_to_mC(uint32_t value)
+{
+	return 147500 - (int)((75U*3300U*value)/4096U);
+}
+
+int adc_convert(uint32_t *value, int channel);
+int adc_convert_ex(uint32_t *values, int *channels, int count);
+
+#endif
+
+/* ADC register offsets ************************************************************/
+
+#define STLR_ADC_ACTSS_OFFSET            0x000
+#define STLR_ADC_EMUX_OFFSET             0x014
+#define STLR_ADC_PSSI_OFFSET             0x028
+#define STLR_ADC_SSMUX0_OFFSET           0x040
+#define STLR_ADC_SSMUX1_OFFSET           0x060
+#define STLR_ADC_SSMUX2_OFFSET           0x080
+#define STLR_ADC_SSMUX3_OFFSET           0x0A0
+#define STLR_ADC_SSCTL0_OFFSET           0x044
+#define STLR_ADC_SSCTL1_OFFSET           0x064
+#define STLR_ADC_SSCTL2_OFFSET           0x084
+#define STLR_ADC_SSCTL3_OFFSET           0x0A4
+#define STLR_ADC_SSFIFO0_OFFSET          0x048
+#define STLR_ADC_SSFIFO1_OFFSET          0x068
+#define STLR_ADC_SSFIFO2_OFFSET          0x088
+#define STLR_ADC_SSFIFO3_OFFSET          0x0A8
+#define STLR_ADC_SSFSTAT0_OFFSET         0x04C
+#define STLR_ADC_SSFSTAT1_OFFSET         0x06C
+#define STLR_ADC_SSFSTAT2_OFFSET         0x08C
+#define STLR_ADC_SSFSTAT3_OFFSET         0x0AC
+
+/* uDMA register addresses **********************************************************/
+
+#define STLR_ADC0_ACTSS                  (STLR_ADC0_BASE + STLR_ADC_ACTSS_OFFSET)
+#define STLR_ADC0_EMUX                   (STLR_ADC0_BASE + STLR_ADC_EMUX_OFFSET)
+#define STLR_ADC0_PSSI                   (STLR_ADC0_BASE + STLR_ADC_PSSI_OFFSET)
+#define STLR_ADC0_SSMUX0                 (STLR_ADC0_BASE + STLR_ADC_SSMUX0_OFFSET)
+#define STLR_ADC0_SSMUX1                 (STLR_ADC0_BASE + STLR_ADC_SSMUX1_OFFSET)
+#define STLR_ADC0_SSMUX2                 (STLR_ADC0_BASE + STLR_ADC_SSMUX2_OFFSET)
+#define STLR_ADC0_SSMUX3                 (STLR_ADC0_BASE + STLR_ADC_SSMUX3_OFFSET)
+#define STLR_ADC0_SSCTL0                 (STLR_ADC0_BASE + STLR_ADC_SSCTL0_OFFSET)
+#define STLR_ADC0_SSCTL1                 (STLR_ADC0_BASE + STLR_ADC_SSCTL1_OFFSET)
+#define STLR_ADC0_SSCTL2                 (STLR_ADC0_BASE + STLR_ADC_SSCTL2_OFFSET)
+#define STLR_ADC0_SSCTL3                 (STLR_ADC0_BASE + STLR_ADC_SSCTL3_OFFSET)
+#define STLR_ADC0_SSFIFO0                (STLR_ADC0_BASE + STLR_ADC_SSFIFO0_OFFSET)
+#define STLR_ADC0_SSFIFO1                (STLR_ADC0_BASE + STLR_ADC_SSFIFO1_OFFSET)
+#define STLR_ADC0_SSFIFO2                (STLR_ADC0_BASE + STLR_ADC_SSFIFO2_OFFSET)
+#define STLR_ADC0_SSFIFO3                (STLR_ADC0_BASE + STLR_ADC_SSFIFO3_OFFSET)
+#define STLR_ADC0_SSFSTAT0               (STLR_ADC0_BASE + STLR_ADC_SSFSTAT0_OFFSET)
+#define STLR_ADC0_SSFSTAT1               (STLR_ADC0_BASE + STLR_ADC_SSFSTAT1_OFFSET)
+#define STLR_ADC0_SSFSTAT2               (STLR_ADC0_BASE + STLR_ADC_SSFSTAT2_OFFSET)
+#define STLR_ADC0_SSFSTAT3               (STLR_ADC0_BASE + STLR_ADC_SSFSTAT3_OFFSET)
+
+/* ADC Active Sample Sequencer (ADCACTSS), offset 0x000 */
+
+#define ADC_ACTSS_ASEN0                  (1 << 0)
+#define ADC_ACTSS_ASEN1                  (1 << 1)
+#define ADC_ACTSS_ASEN2                  (1 << 2)
+#define ADC_ACTSS_ASEN3                  (1 << 3)
+#define ADC_ACTSS_BUSY                   (1 << 16)
+
+/* ADC Event Multiplexer Select (ADCEMUX), offset 0x014 */
+
+/* ADC Processor Sample Sequence Initiate (ADCPSSI), offset 0x028 */
+
+#define ADC_PSSI_SS0                     (1 << 0)
+#define ADC_PSSI_SS1                     (1 << 1)
+#define ADC_PSSI_SS2                     (1 << 2)
+#define ADC_PSSI_SS3                     (1 << 3)
+#define ADC_PSSI_SYNCWAIT                (1 << 27)
+#define ADC_PSSI_GSYNC                   (1 << 31)
+
+/* ADC Sample Sequence FIFO Status */
+
+#define ADC_SSFSTAT_EMPTY                (1 << 8)
+#define ADC_SSFSTAT_FULL                 (1 << 12)
+
+/* SSCTL */
+#define ADC_SSCTL_D                      (1 << 0)
+#define ADC_SSCTL_END                    (1 << 1)
+#define ADC_SSCTL_IE                     (1 << 2)
+#define ADC_SSCTL_TS                     (1 << 3)
+
+#define ADC_SSFIFO_DATA_MASK             0x0FFF
+
+#endif /* __MACH_PLAT_STELLARIS_ADC_H */
